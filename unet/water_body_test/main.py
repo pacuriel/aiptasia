94b39d512be75ea1ef6ae60ac3b53832bea1369b
pacuriel <pcuriel@ucmerced.edu>
2024-09-25 17:34:42 -0700

    updated gitignore. tested running u-net in main. started modified version of u-net. updating mask to binary.

--- unet/water_body_test/main.py
+++ unet/water_body_test/main.py
@@ -5,9 +5,10 @@ September 2024
 """
 import platform
 import os
+import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from unet_OG import UNet #importing OG U-Net Model
@@ -15,6 +16,15 @@ from data import WaterBodyDataset #dataset class
 from train import Train
 
 RANDOM_SEED = 42 #random seed for testing (not so random i guess?)
+
+#setting hyperparameters as global variables
+#TODO: don't hardcode hyperparams
+in_channels = 3 #three channels for RGB images
+out_channels = 1
+num_class = 2
+learning_rate = 0.001 #learning rate used by optimizer
+batch_size = 8 #size of each batch to train on
+num_epochs = 10 #number of epochs (full passes over training data) to train for 
 
 #setting global variables
 device = "cuda" if torch.cuda.is_available() else "cpu" #device for pytorch
@@ -63,13 +73,33 @@ def main():
     train_set = train_test_splits[0] 
     test_set = train_test_splits[1]
 
+    #preparing data for training with dataloaders
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
+    model = UNet(in_channels=in_channels, out_channels=out_channels).to(device) #initializing model
+    loss_fcn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    #looping for set number of epochs
+    for epoch in range(num_epochs):
+        print(f"*** Current epoch: {epoch + 1}") #sanity check
+        breakpoint()
+        #looping over batches of data
+        for batch_idx, (img_batch, mask_batch) in enumerate(tqdm(train_loader)):
+            img_batch = img_batch.to(device) #batch of images
+            gt_masks = mask_batch.float().unsqueeze(1).to(device) #batch of masks
+
+            pred_masks = model(img_batch) #forward pass
+            loss = loss_fcn(pred_masks, gt_masks) #calculating loss
+
+            #backward pass (backprop)
+            optimizer.zero_grad() #initializing gradients to zero
+            loss.backward() #update weights depending on gradients computer above
+            optimizer.step() #gradient descent (or ADAM step)
+    
     breakpoint()
     exit()
-    model = UNet(in_channels=3, out_channels=2).to(device) #initializing model
-    loss = nn.CrossEntropyLoss()
-    # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     
     breakpoint()
     for i, (image, mask) in enumerate(dataset):
