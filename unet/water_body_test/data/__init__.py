b06cefa8286d2000d3e86c8866b880fedcbb7759
Pablo Curiel <pcuriel@rclogin01.cluster>
2024-10-02 13:40:50 -0700

    removed water body directory

--- unet/water_body_test/data/__init__.py
+++
@@ -1,2 +0,0 @@
-from .dataset import WaterBodyDataset
-from .data_loader import WaterBodyDataLoader
--- unet/water_body_test/data/data_loader.py
+++
@@ -1,9 +0,0 @@
-from torch.utils.data import DataLoader
-
-class WaterBodyDataLoader():
-    def __init__(self, dataset):
-        self.dataset = dataset
-
-        data_loader = DataLoader(dataset=dataset)
-
-        
