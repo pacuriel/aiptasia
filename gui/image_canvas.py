dd51fc7231ad96410d6f25f5de9bbe668d4be623
Pablo Curiel <pcuriel@ucmerced.edu>
2025-01-24 13:40:53 -0800

    Loading image in image canvas class

--- gui/image_canvas.py
+++ gui/image_canvas.py
@@ -13,27 +13,37 @@ class ImageCanvas:
         self.image_file = image_file
 
         # Filter to apply when displaying image (NEAREST = original, LANCZOS = antialias)
-        self.__filter = Image.NEAREST 
-        # self.__filter = Image.LANCZOS
+        # self.__filter = Image.NEAREST 
+        self.__filter = Image.LANCZOS
 
         self.__previous_state = 0 # Previous state of the key presses
 
         # Creating Canvas and binding keys to events
-        self.canvas = tk.Canvas(self.master, background="black", cursor="spider")
+        self.canvas = tk.Canvas(self.master, background="gray12", cursor="spider")
         self.canvas.grid(row=0, column=0, sticky='nswe') # Displaying Canvas in grid of parent
         self.canvas.update() # Updating canvas
-
         self.__bind_events() # Calling function to bind keys to events
+
+        # Loading image and storing relevant details
+        self.pil_image = Image.open(self.image_file)
+        self.img_width, self.img_height = self.pil_image.size # Image height/width
+        self.__min_side = min(self.img_height, self.img_width) # Smallest side of image
+
+        # Creating rectangle container to store/track image on Canvas
+        self.img_container = self.canvas.create_rectangle(0, 0, self.img_width, self.img_height, width=0)
+
+        breakpoint()
 
     # Binds keys to events
     def __bind_events(self) -> None:
             self.canvas.bind('<Button-1>', self.button_1_test)
     
     # Function to test key bidnings
-    def button_1_test(self, event):
+    def button_1_test(self, event) -> None:
         print(f"Button 1 (LMB) pressed at {event.x, event.y}") # Sanity check
         self.__old_event = event # Updating previous event
 
-    # Places ImageCanvas 
-    def grid(self, **kwargs):
+    # Displays image on canvas
+    def __display_image(self):
+        
         pass
