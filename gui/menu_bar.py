c3446288559265448e3740a032822c4a6229585b
Pablo Curiel <pcuriel@ucmerced.edu>
2025-02-14 11:57:33 -0800

    Started adding more menu bar options.

--- gui/menu_bar.py
+++ gui/menu_bar.py
@@ -18,16 +18,27 @@ class MenuBar(tk.Menu):
         self.master = master # Parent widget
         self.helper_funcs = helper_funcs # Helper functionss
 
-        self.__create_menu() # Creating menu bar
+        self.__create_menus() # Creating menu bar
 
-    def __create_menu(self) -> None:
+    def __create_menus(self) -> None:
         """Creates menu bar and options."""
         # File button and dropdown options
-        self.file_menu = tk.Menu(master=self, tearoff=tk.OFF)
-        self.add_cascade(label='File', menu=self.file_menu, underline=0)
-        self.file_menu.add_command(label="Open Image", underline=0, command=self.helper_funcs["open_image"])
+        self.file = tk.Menu(master=self, tearoff=tk.OFF)
+        self.add_cascade(label='File', menu=self.file, underline=0)
+        self.file.add_command(label="Open Image", underline=0, command=self.helper_funcs["open_image"])
         
-        self.file_menu.add_command(label="Exit", underline=1, command=self.quit) # Exits application
+        self.file.add_command(label="Exit", underline=1, command=self.quit) # Exits application
+        
+        # View button
+        self.view = tk.Menu(master=self, tearoff=tk.OFF)
+        self.add_cascade(label="View", menu=self.view, underline=0)
+        self.view.add_cascade(label="Fullscreen", )#, command=self.helper_funcs["fullscreen"])
+
+        # Mode button
+        self.mode = tk.Menu(master=self, tearoff=tk.OFF)
+        self.add_cascade(label="Mode", menu=self.mode, underline=0)
+        self.mode.add_cascade(label="Prompting", underline=0, command=self.helper_funcs["prompting_mode"])
+
 
     # Exits application
     def quit_app(self) -> None: 
