aaddbc649e02b521436df60ad6f40a02f4b01f9f
Pablo Curiel <pcuriel@ucmerced.edu>
2025-02-18 11:34:23 -0800

    Started logger class

--- gui/logger.py
+++ gui/logger.py
@@ -1,4 +1,8 @@
 import os
+import logging
+import sys
+
+
 
 class Logger:
     """Class responsible for logging info relevant to application/user."""
@@ -11,7 +15,31 @@ class Logger:
         self.log_dir = log_dir # Directory to save log files
         self.__create_log_dir() # Creating log_directory
 
+        self.__configure_logger() # Configuring logger
+
+    def __configure_logger(self):
+        logging.basicConfig(level=logging.INFO,
+                            format='%(levelname)s, %(asctime)s, %(filename)s, %(funcName)s, %(message)s',
+                            filename=f'{self.log_dir}/logfile.log',
+                            filemode='a')
+        
+        console = logging.StreamHandler() # Handler to write to sys.stdout
+        formatter = logging.Formatter('[%(levelname)s] %(message)s') # Format for console output
+        console.setFormatter(fmt=formatter) # Assigning format to handler
+        logging.getLogger(name=None).addHandler(console) # Adding handler to root logger
+
+        sys.excepthook = self.__handle_uncaught_exception
+
     def __create_log_dir(self) -> None:
         """Creates log directory if it doesn't exist yet."""
         if not os.path.isdir(self.log_dir):
             os.makedirs(self.log_dir)
+
+    def __handle_uncaught_exception(self, exc_type, exc_value, exc_traceback):
+        """Handles uncaught exceptions and displays in logger."""
+        if issubclass(exc_type, KeyboardInterrupt):
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
+            return
+
+        logger = logging.getLogger(name=None)
+        logger.error('Uncaught exception occured', exc_info=(exc_type, exc_value, exc_traceback))
--- gui/run_main.py
+++ gui/run_main.py
@@ -1,12 +1,18 @@
 """Code obtained and adapted from: https://github.com/foobar167/junkyard/tree/master/manual_image_annotation1."""
+import logging
+
 from main_window import MainWindow
+from logger import Logger
 
 def main(): 
     """Main function"""
+    # Initalizing logger
+    logger = Logger()
+    logging.info("Starting application") # Startup message
+
     main_window = MainWindow() # Storing MainWindow object
     main_window.mainloop() # Running application
-
-    ### Use logger? 
+    logging.info("Exiting application") # Exit message
 
 if __name__ == "__main__":
     main()
